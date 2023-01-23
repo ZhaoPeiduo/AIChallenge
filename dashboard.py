796b47d80703c0a1bf14522ec365f02548e70ba5
ZhaoPeiduo <e0389118@u.nus.edu>
2023-01-23 13:16:43 +0800

    Refactor

--- dashboard.py
+++ dashboard.py
@@ -18,13 +18,25 @@ class SampleApp:
                 data_dict["count"][label + 1] += 1
         return data_dict
 
+    # def load_statistics(self):
+    #     df = pd.read_csv("./outputs/data.csv")
+    #     total_likes = df["Likes"].fillna(0).sum()
+    #     total_comments = df["Comments"].fillna(0).sum()
+    #     total_retweets = df["Retweets"].fillna(0).sum()
+    #     return total_likes, total_comments, total_retweets
+    #
+    # total_likes, total_comments, total_retweets = self.load_statistics()
+    # Output('total comments', 'total comments'),
+    # Output('total likes', 'total likes'),
+    # Output('total retweets', 'total retweets')]
+    # dash_table.DataTable(df.to_dict('records'), [{"name": i, "id": i} for i in df.columns])
+
     def generate_chart(self, num_of_days, word, limit=40, driver_type="chrome"):
         self._runner = runner.Runner(num_of_days, word, limit, driver_type)
         model_result = self._runner()
         df = pd.DataFrame.from_dict(self.load_data(model_result))
         fig = px.pie(df, values='count', names=[-1, 0, 1], title='sentiment distribution')
-        total_likes, total_comments, total_retweets = self.load_statistics()
-        return fig, total_likes, total_comments, total_retweets
+        return fig
 
     def run_app(self):
         app = Dash(__name__)
